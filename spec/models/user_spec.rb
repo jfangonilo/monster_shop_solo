a9acba0bad2fbd4bf82a8a83243df00957f1fb56
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-23 23:54:13 -0700

    Add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :email }
+  end
+end

5510c4fbb793450ae78808e3b2751d958ef1d4a1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-23 23:32:31 -0700

    Add route for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
 
+  get "/login", to: "sessions#new"
+
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"

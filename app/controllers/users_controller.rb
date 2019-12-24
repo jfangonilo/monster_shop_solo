17856dfcf2b7448ee840286b2b6931556d91d1a5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 01:19:36 -0700

    Add strong params and profile redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    redirect_to '/profile'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
 end

ca49c4f82ebfa5a52513702b7daf4c3948c648a5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 14:19:42 -0700

    Add spec for merchant employee login

--- spec/features/users/login_logout_spec.rb
+++ spec/features/users/login_logout_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "User Login and Logout" do
-  describe "As a regular user"
+  describe "As a regular user" do
     before :each do
       @user = create(:random_user)
     end
@@ -16,4 +16,22 @@ RSpec.describe "User Login and Logout" do
       expect(current_path).to eq "/profile"
       expect(page).to have_content "You are logged in"
     end
+  end
+
+  describe "As a merchant employee" do
+    before :each do
+      @merchant_employee = create(:merchant_employee)
+    end
+
+    it "I can log in" do
+      visit "/login"
+
+      fill_in "Email", with: @merchant_employee.email
+      fill_in "Password", with: @merchant_employee.password
+
+      click_button "Log In"
+      expect(current_path).to eq "/merchant"
+      expect(page).to have_content "You are logged in"
+    end
+  end
 end

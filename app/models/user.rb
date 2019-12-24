364826d52d121afb734363d0ee609f1fcc4a8e75
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 01:42:59 -0700

    Fix email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, uniqueness: true, case_sensitive: false
 
   validates_presence_of :name, :address, :city, :state, :zip, :email
   validates_numericality_of :zip
-  validates_uniqueness_of :email, case_sensitive: false
   validates_confirmation_of :password
 end

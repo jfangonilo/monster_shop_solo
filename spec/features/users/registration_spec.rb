de5257b63fb28c0d4a860f9808c08546012ab24a
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 10:34:47 -0700

    Add spec for registration unique email

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -48,5 +48,26 @@ RSpec.describe 'User Registration' do
       expect(page).to have_content "Password can't be blank"
       expect(page).to have_content "Password confirmation doesn't match Password"
     end
+
+    it "I must use a unique email" do
+      user = create(:random_user)
+
+      visit '/'
+      within 'nav' do
+        click_link 'Register'
+      end
+
+      fill_in :user_name, with: user.name
+      fill_in :user_address, with: user.address
+      fill_in :user_city, with: user.city
+      fill_in :user_state, with: user.state
+      fill_in :user_zip, with: user.zip
+      fill_in :user_email, with: user.email
+      fill_in :user_password, with: user.password
+      fill_in :user_password_confirmation, with: user.password
+      click_button 'Submit'
+
+      expect(page).to have_content "Email has already been taken"
+    end
   end
 end

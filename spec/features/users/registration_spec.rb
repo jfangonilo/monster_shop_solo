f7f2c1e7d0766846040a89e6dfd45a5f6671ae9f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 00:37:30 -0700

    Add spec for registering

---
+++ spec/features/users/registration_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'User Registration' do
+  describe 'As a visitor' do
+    it 'I can see a link to register' do
+      visit '/'
+      within 'nav' do
+        click_link "Register" do
+      end
+      expect(current_path).to eq '/'
+      end
+    end
+  end
+end

b8a7fe141c2a93aa34580837f7c3ce32a31ffd0e
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 00:38:55 -0700

    Add route to register

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
 
+  get "/register", to: "users#new"
+
   get "/login", to: "sessions#new"
 
   get "/merchants", to: "merchants#index"

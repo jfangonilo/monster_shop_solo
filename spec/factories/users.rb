0f601834972401c2c86e7ba7507912be4e64822d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 14:11:39 -0700

    Add other roles to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,42 @@ FactoryBot.define do
     address   {Faker::Address.street_address}
     city      {Faker::Address.city}
     state     {Faker::Address.state_abbr}
-    zip       {12345}
-    email     {Faker::Internet.email}
+    zip       {Faker::Address.zip}
+    email     {"user@mail.com"}
     password  {"1234"}
     role      {0}
+  end
+
+  factory :merchant_employee, class: User do
+    name      {Faker::FunnyName.name}
+    address   {Faker::Address.street_address}
+    city      {Faker::Address.city}
+    state     {Faker::Address.state_abbr}
+    zip       {Faker::Address.zip}
+    email     {"merchant_employee@mail.com"}
+    password  {"1234"}
+    role      {1}
+  end
+
+  factory :merchant_admin, class: User do
+    name      {Faker::FunnyName.name}
+    address   {Faker::Address.street_address}
+    city      {Faker::Address.city}
+    state     {Faker::Address.state_abbr}
+    zip       {Faker::Address.zip}
+    email     {"merchant_admin@mail.com"}
+    password  {"1234"}
+    role      {2}
+  end
+
+  factory :admin, class: User do
+    name      {Faker::FunnyName.name}
+    address   {Faker::Address.street_address}
+    city      {Faker::Address.city}
+    state     {Faker::Address.state_abbr}
+    zip       {Faker::Address.zip}
+    email     {"admin@mail.com"}
+    password  {"1234"}
+    role      {3}
   end
 end

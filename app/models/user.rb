c122f494f7c6f415cc12a9189a7989d1c77001c9
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 01:15:34 -0700

    Add password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :email
   validates_numericality_of :zip
   validates_uniqueness_of :email
+  validates_confirmation_pf :password
 end

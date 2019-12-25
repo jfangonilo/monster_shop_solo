2ece9a6c9b2dfd9bebffd77a512a9dcbd310777f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-24 23:15:16 -0700

    Add merchant navigation spec

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -75,4 +75,29 @@ RSpec.describe 'Site Navigation' do
       expect(page).to have_content "Logged in as #{@user.name}"
     end
   end
+
+  describe "As a merchant user" do
+    before :each do
+      @merchant_employee = create(:merchant_employee)
+      @merchant_admin = create(:merchant_admin)
+    end
+
+    it "I can see a link to the merchant dash" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
+      visit "/"
+
+      expect(page).to have_link "Home"
+      expect(page).to have_link "All Merchants"
+      expect(page).to have_link "All Items"
+      expect(page).to have_link "Cart"
+      expect(page).to have_link "Log Out"
+      expect(page).not_to have_link "Register"
+      expect(page).not_to have_link "Log In"
+      within 'nav' do
+        click_link "Merchant Dashboard"
+      end
+      expect(current_path).to eq "/merchant"
+      expect(page).to have_content "Logged in as #{@merchant_employee.name}"
+    end
+  end
 end

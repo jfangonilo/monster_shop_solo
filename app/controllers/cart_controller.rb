eebdb561dc2fe571b063e4d2701d4e7d4fef6f10
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-25 00:12:38 -0700

    Add exclude admin filter to cart show

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,6 @@
 class CartController < ApplicationController
   before_action :exclude_merchant, only: [:show]
+  before_action :exclude_admin, only: [:show]
 
   def add_item
     item = Item.find(params[:item_id])

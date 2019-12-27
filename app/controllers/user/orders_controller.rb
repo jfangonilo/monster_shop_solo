9ede2841655b56c57d382e0f29b34b0b3ba9d2eb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-26 22:37:19 -0700

    Update show action to only allow user orders

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -5,7 +5,7 @@ class User::OrdersController < User::BaseController
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = current_user.orders.find(params[:id])
   end
 
   def create

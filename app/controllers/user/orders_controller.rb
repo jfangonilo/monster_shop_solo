a60be1fde118ce0993d9b2ce141fd09a4d81c548
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-28 14:35:00 -0700

    Add move order cancel to model

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -34,11 +34,7 @@ class User::OrdersController < User::BaseController
 
   def update
     order = current_user.orders.find(params[:id])
-    order.update(status: "cancelled")
-    order.item_orders.each do |item_order|
-      item_order.update(status: "unfulfilled")
-      item_order.item.update(inventory: item_order.item.inventory + item_order.quantity)
-    end
+    order.cancel
     redirect_to profile_path
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -15,6 +15,14 @@ class Order <ApplicationRecord
     item_orders.sum(:quantity)
   end
 
+  def cancel
+    update(status: "cancelled")
+    item_orders.each do |item_order|
+      item_order.update(status: "unfulfilled")
+      item_order.item.update(inventory: item_order.item.inventory + item_order.quantity)
+    end
+  end
+
   def package_if_fulfilled
     update(status: "packaged") if item_orders.distinct.pluck(:status) == ["fulfilled"]
   end

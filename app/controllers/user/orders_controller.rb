d62efc90f527e9ae2eb7720b18fc593375311221
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-28 00:18:09 -0700

    Optimize query to include item.merchant

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -9,7 +9,7 @@ class User::OrdersController < User::BaseController
   end
 
   def show
-    @order = current_user.orders.find(params[:id])
+    @order = current_user.orders.includes(items: [:merchant]).find(params[:id])
   end
 
   def create

4e2c2f51b9ec2e89665158ab08a2f79dd813f153
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-29 10:46:58 -0700

    Remove item destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create]
   end
 
-  resources :items, except: [:new, :create] do
+  resources :items, except: [:new, :create, :destroy] do
     resource :reviews, only: [:new, :create]
   end
 

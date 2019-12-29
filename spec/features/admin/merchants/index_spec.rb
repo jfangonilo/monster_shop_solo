1f52ce68c7dd42b05edb2f9d6064cf47d65dddd5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-28 23:05:57 -0700

    Add spec for disable merchant

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "admin merchant index" do
+  before :each do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    @user = create(:random_user)
+    @merchant = create(:random_merchant)
+    @item = create(:random_item, merchant: @merchant)
+    @order = create(:random_order, user: @user)
+    create(:item_order, item: @item, order: @order, price: @item.price)
+  end
+
+  it 'allows me to disable a merchant' do
+    visit "/merchants"
+
+    within "#merchant-#{@merchant.id}" do
+      click_button "Disable Merchant"
+    end
+
+    expect(current_path).to eq("/merchants")
+    expect(page).to have_content("#{@merchant.name} disabled")
+    @merchant.reload
+    expect(@merchant.active).to be(false)
+  end
+end

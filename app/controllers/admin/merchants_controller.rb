15c56eb0833c8c00032b24d4e4a3842830e27517
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-28 23:16:16 -0700

    Add update to merchant items

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,6 +6,7 @@ class Admin::MerchantsController < Admin::BaseController
   def update
     merchant = Merchant.find(params[:id])
     merchant.toggle!(:active)
+    merchant.items.update_all(active?: false)
     flash[:success] = "#{merchant.name} disabled"
     redirect_to merchants_path
   end

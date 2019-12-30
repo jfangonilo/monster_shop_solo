65479a6a27a732d8417d7f3b8d88a9912f1e54bf
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-30 10:30:20 -0700

    Add spec for edit page link

---
+++ spec/features/merchant/items/edit_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "merchant items edit page" do
+  before :each do
+    @merchant = create(:random_merchant)
+    @merchant_user = create(:merchant_employee, merchant: @merchant)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_user)
+
+    @item = create(:random_item, merchant: @merchant)
+  end
+
+  it 'can be accessed from the merchant items index page' do
+    visit "/merchant/items"
+    within "#item-#{@item.id}" do
+      expect(page).to have_button("Edit Item")
+    end
+
+    expect(current_path).to eq("/merchant/items/edit")
+  end
+end

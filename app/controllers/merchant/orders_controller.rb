7179a61428ff289ce6c440351e79b39fa924891f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-30 19:13:12 -0700

    Refactor show action

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,7 +1,8 @@
 class Merchant::OrdersController < Merchant::BaseController
 
   def show
-    @order = current_user.merchant.orders.find(params[:id])
+    @merchant = current_user.merchant
+    @order = @merchant.orders.find(params[:id])
   end
 
 end

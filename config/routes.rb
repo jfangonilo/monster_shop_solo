96d1c2e3941de530999f79f023936d0cf0143b7c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-31 10:20:35 -0700

    Add route for admin user order show

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,8 @@ Rails.application.routes.draw do
     get "/", to: "dashboard#index"
     resources :orders, only: [:update]
     resources :merchants, only: [:show, :update]
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show] do
+      resources :orders, only: [:show]
+    end
   end
 end

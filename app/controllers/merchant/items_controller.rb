0d1aa3f40160af351a2bbab7e35edba58cca33f3
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-31 16:08:47 -0700

    Update to use eager loading

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,7 +2,7 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def index
     @merchant = current_user.merchant
-    @items = @merchant.items
+    @items = @merchant.items.includes(:item_orders)
   end
 
   def new
--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -3,7 +3,7 @@ class Merchant::OrdersController < Merchant::BaseController
   def show
     merchant = current_user.merchant
     @order = merchant.orders.find(params[:id])
-    @item_orders = merchant.item_orders_from_order(@order)
+    @item_orders = merchant.item_orders_from_order(@order).includes(:item)
   end
 
   def update

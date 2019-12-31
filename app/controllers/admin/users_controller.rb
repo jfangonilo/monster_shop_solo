2dfec6e8cfdd5167bdc00e44d0e51a6b366c42f7
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-31 12:24:36 -0700

    Add update action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,4 +12,21 @@ class Admin::UsersController < Admin::BaseController
     @display_user = User.find(params[:id])
   end
 
+  def update
+    @display_user = User.find(params[:id])
+    if @display_user.update(user_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to "/admin/users/#{@display_user.id}"
+    else
+      flash_errors(@display_user)
+      render :edit
+    end
+  end
+
+private
+
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+  end
+
 end

11b00bc816aabc497b9dd2369452984780d842d8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-30 20:14:31 -0700

    Update link route

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -6,4 +6,11 @@ class Merchant::OrdersController < Merchant::BaseController
     @item_orders = merchant.item_orders_from_order(@order)
   end
 
+  def update
+    merchant = current_user.merchant
+    order = merchant.orders.find(params[:id])
+    item_orders = merchant.item_orders_from_order(order)
+    binding.pry
+  end
+
 end

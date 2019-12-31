aadc46cf6c858adb2935f5a9690893cc423fcd1c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-31 11:24:12 -0700

    Add user order show spec

--- spec/features/admin/orders/show_spec.rb
+++ spec/features/admin/orders/show_spec.rb
@@ -41,4 +41,54 @@ RSpec.describe "admin user order show page" do\
     end
     expect(current_path).to eq("/admin/users/#{@user_1.id}/orders/#{@order_3.id}")
   end
+
+  it 'shows all info about the order' do
+    order = create(:random_order, user: @user_1)
+    merchant = create(:random_merchant)
+
+    item_1 = create(:random_item, price: 10, merchant: merchant)
+    item_order_1 = create(:item_order, item: item_1, order: order, price: item_1.price, quantity: 4)
+
+    item_2 = create(:random_item, price: 1, merchant: merchant)
+    item_order_2 = create(:item_order, item: item_2, order: order, price: item_2.price, quantity: 2)
+
+    item_3 = create(:random_item, price: 7, merchant: merchant)
+    item_order_3 = create(:item_order, item: item_3, order: order, price: item_3.price, quantity: 5)
+
+    visit "admin/users/#{@user_1.id}/orders/#{order.id}"
+
+    within "#order-data" do
+      expect(page).to have_content("Order ID: #{order.id}")
+      expect(page).to have_content("#{order.created_at}")
+      expect(page).to have_content("#{order.updated_at}")
+      expect(page).to have_content("#{order.status.capitalize}")
+    end
+
+    within "#item-#{item_1.id}" do
+      expect(page).to have_content("#{item_1.name}")
+      expect(page).to have_content("#{item_1.description}")
+      expect(page).to have_css("img[src*='#{item_1.image}']")
+      expect(page).to have_content("#{item_1.price}")
+      expect(page).to have_content("#{item_order_1.quantity}")
+      expect(page).to have_content("#{item_order_1.subtotal}")
+    end
+
+    within "#item-#{item_2.id}" do
+      expect(page).to have_content("#{item_2.name}")
+      expect(page).to have_content("#{item_2.description}")
+      expect(page).to have_css("img[src*='#{item_2.image}']")
+      expect(page).to have_content("#{item_2.price}")
+      expect(page).to have_content("#{item_order_2.quantity}")
+      expect(page).to have_content("#{item_order_2.subtotal}")
+    end
+
+    within "#item-#{item_3.id}" do
+      expect(page).to have_content("#{item_3.name}")
+      expect(page).to have_content("#{item_3.description}")
+      expect(page).to have_css("img[src*='#{item_3.image}']")
+      expect(page).to have_content("#{item_3.price}")
+      expect(page).to have_content("#{item_order_3.quantity}")
+      expect(page).to have_content("#{item_order_3.subtotal}")
+    end
+  end
 end

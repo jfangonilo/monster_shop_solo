a81bb94c5606eed02b12f56b3f2ca1820238b566
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-30 20:30:09 -0700

    Add fulfill action

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -9,8 +9,11 @@ class Merchant::OrdersController < Merchant::BaseController
   def update
     merchant = current_user.merchant
     order = merchant.orders.find(params[:id])
-    item_orders = merchant.item_orders_from_order(order)
-    binding.pry
+    item_order = order.item_orders.find(params[:item_order_id])
+    item_order.update(status: "fulfilled")
+    item_order.item.update(inventory: item_order.item.inventory - item_order.quantity)
+    order.package_if_fulfilled
+    redirect_to merchant_dash_order_path(order)
   end
 
 end

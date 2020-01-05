a1fa7c4de2a405111709fdf0ebc7cf5a95f31cb8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-04 21:03:43 -0700

    Add migration for user active column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191229190717) do
+ActiveRecord::Schema.define(version: 20200105040121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20191229190717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
+    t.boolean "active", default: true
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
